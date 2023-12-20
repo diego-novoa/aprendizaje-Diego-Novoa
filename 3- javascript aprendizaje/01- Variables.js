cb0379dc9bde5101cf33f48fe5d374ac6df03f52
“DIEGO <[200~64092270+diego-novoa@users.noreply.github.com>
2023-12-19 21:19:31 -0500

    ok

--- 3- javascript aprendizaje/01- Variables.js
+++ 3- javascript aprendizaje/01- Variables.js
@@ -1,3 +1,40 @@
 // Una Variable es un espacio
-// de memoria que contiene un valor (let,const)
+// de memoria que contiene un valor (var, let,const)
+
+// declaracion de variable
+let mascota;
+
+// inicializacion de una variable
+
+mascota = "perro";
+
+// declaracion e inicializacion
+//de una variable 
+
+let mascota1 = "perro";
+
+// REGLAS DE LAS VARIABLES
+/*
+1. let no me permite declarar la variable
+   con el mismo nombre.
+
+2. let permite declarar la variable
+y no inicializarla
+mas tarde en cualquier lugar del 
+codigo volver a inicializarlo.
+
+1. var permite declarar una variable
+con el mismo nombre
+2. var me permite declarar sin 
+inicializar variable
+
+*/
+
+/*var permite declarar una variable
+con el mismo nombre*/
+
+var nomre = "perro";
+var nomre = "carlos";
+
+// variable undefine o variable null
 
