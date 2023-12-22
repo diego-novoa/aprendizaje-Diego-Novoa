bcccaf82c3013379a9e0e950981a8cd8583980f7
“DIEGO <[200~64092270+diego-novoa@users.noreply.github.com>
2023-12-21 22:00:42 -0500

    ok

---
+++ 3- javascript aprendizaje/02- Tipos datos.js
@@ -0,0 +1,16 @@
+// Objetos (objetos y arrays)
+
+const persona = {
+  nombre:"Maria",
+  apellidos:"Gonzales perez",
+  edad:32
+}
+// tipo de dato objeto
+
+// tipo dato arrays o listas
+
+
+
+
+
+// primitivos
