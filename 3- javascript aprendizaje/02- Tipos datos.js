df1d361bb2a051f71adc62a6970a73c4965278b5
“DIEGO <[200~64092270+diego-novoa@users.noreply.github.com>
2023-12-21 22:16:40 -0500

    ok

--- 3- javascript aprendizaje/02- Tipos datos.js
+++ 3- javascript aprendizaje/02- Tipos datos.js
@@ -9,8 +9,21 @@ const persona = {
 
 // tipo dato arrays o listas
 
-
-
-
+const frutas = ["pera","limon"]
 
 // primitivos
+//(numbers,floats,strings,boolean,undefined,null)
+
+const edad=23 // tipo dato number
+const grados=4.7 // tipo dato float
+const nombre="Maria" // tipo dato string
+const isBlue=false // tipo dato boolean
+
+let perro; // tipo de dato undefined
+let emfermo=null // tipo de dato null
+
+// TYPEOF
+// con typeof vamos a comprobar el 
+//tipo de dato con el que estamos trabajando
+
+console.log(typeof perro)
