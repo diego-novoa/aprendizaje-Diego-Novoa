df64a3593b40cdb41d0e03ad92cebdd07aa86621
“DIEGO <[200~64092270+diego-novoa@users.noreply.github.com>
2023-12-22 22:37:28 -0500

    ok

---
+++ 3- javascript aprendizaje/03- Ambito Variables.js
@@ -0,0 +1,32 @@
+// AMBITO SUPERGLOBAL
+// Tenemos acceso 
+// desde cualquier archivo
+
+// AMBITO GLOBAL***************
+// es el contexto que 
+// engloba un mismo archivo
+
+// VAR
+// Esta en un archivo libre 
+//no esta dentro de una funcion
+// no esta dentro de un if, 
+//ni de una sentencia que tenga 
+//llaves esta libre fuera.
+
+var perro= "lucas";
+
+// AMBITO LOCAL****************
+// tenemos acceso que engloba
+// una funcion
+
+function saltar(){
+  var gato= "felino"
+}
+
+//AMBITO DE BLOQUE**************
+// Es el contexto que esta 
+// englobado dentro de un bucle
+// o condicional if
+
+
+
