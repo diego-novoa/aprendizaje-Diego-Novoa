20746c44f3e3f79083b3dffa89b37ad43aa8e0a0
“DIEGO <[200~64092270+diego-novoa@users.noreply.github.com>
2024-01-05 21:01:36 -0500

    ok

--- 3- javascript aprendizaje/05- Concatenacion.js
+++ 3- javascript aprendizaje/05- Concatenacion.js
@@ -1,4 +1,13 @@
-// 
+// unir una frase o una 
+// parte de un codigo con otro
+ 
 
-// 
-// 
+
+// concatenacion Tradicional 
+
+let frase= 'Me gusta mucho pasear'
+let frase2= ' , por laplaya'
+let frasetotal=frase +frase2;
+
+console.log(frasetotal)
+
