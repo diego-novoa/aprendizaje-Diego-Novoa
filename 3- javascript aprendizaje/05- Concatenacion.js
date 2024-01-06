4c893cb2b9d26bc074860ee401dc1c7c711d0a8c
“DIEGO <[200~64092270+diego-novoa@users.noreply.github.com>
2024-01-05 21:15:55 -0500

    ok

--- 3- javascript aprendizaje/05- Concatenacion.js
+++ 3- javascript aprendizaje/05- Concatenacion.js
@@ -11,11 +11,13 @@ let frasetotal=frase +frase2;
 console.log(frasetotal)
 document.write(frasetotal)
 
-// let sitio='al cine'
-// let frase3= `Me gusta ir mucho ${sitio}` 
+let sitio4= 'al campo'
+let frase5='Megusta ir mucho ' + sitio4 + ' por la noche'
+console.log(frase5)
 
-let sitio = 'al cine'
-let frase3='Megusta ir mucho' + sitio
+// concatenacion moderna
+
+let sitio='al cine'
+let frase3= `Me gusta ir mucho ${sitio}`
+
 console.log(frase3)
-
-
